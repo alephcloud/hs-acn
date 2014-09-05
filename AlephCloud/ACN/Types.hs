dc5774534c66bcdf2cddc8563e2fd21166aa37c8
Vincent Hanquez <vhanquez@alephcloud.com>
2014-09-05 21:07:35 +0100

    add support for tuples in ACN classes.

--- AlephCloud/ACN/Types.hs
+++ AlephCloud/ACN/Types.hs
@@ -64,3 +64,59 @@ instance FromACN a => FromACN [a] where
                                             Right (vals, r') -> Right (a : vals, r')
                                             Left err         -> Left err
     fromACN _ = Left "expecting AcnSetStart"
+
+instance (ToACN a1, ToACN a2) => ToACN (a1,a2) where
+    toACN (a1,a2) = [AcnSeqStart] ++ toACN a1 ++ toACN a2 ++ [AcnSeqEnd]
+instance (FromACN a1, FromACN a2) => FromACN (a1,a2) where
+    fromACN (AcnSeqStart:r) = do
+        (a1, r1) <- fromACN r
+        (a2, r') <- fromACN r1
+        case r' of
+            AcnSeqEnd:l -> return ((a1,a2), l)
+            _           -> Left "expecting AcnSeqEnd"
+instance (ToACN a1, ToACN a2, ToACN a3) => ToACN (a1,a2,a3) where
+    toACN (a1,a2,a3) = [AcnSeqStart] ++ toACN a1 ++ toACN a2 ++ toACN a3 ++ [AcnSeqEnd]
+instance (FromACN a1, FromACN a2, FromACN a3) => FromACN (a1,a2,a3) where
+    fromACN (AcnSeqStart:r) = do
+        (a1, r1) <- fromACN r
+        (a2, r2) <- fromACN r1
+        (a3, r') <- fromACN r2
+        case r' of
+            AcnSeqEnd:l -> return ((a1,a2,a3), l)
+            _           -> Left "expecting AcnSeqEnd"
+instance (ToACN a1, ToACN a2, ToACN a3, ToACN a4) => ToACN (a1,a2,a3,a4) where
+    toACN (a1,a2,a3,a4) = [AcnSeqStart] ++ toACN a1 ++ toACN a2 ++ toACN a3 ++ toACN a4 ++ [AcnSeqEnd]
+instance (FromACN a1, FromACN a2, FromACN a3, FromACN a4) => FromACN (a1,a2,a3,a4) where
+    fromACN (AcnSeqStart:r) = do
+        (a1, r1) <- fromACN r
+        (a2, r2) <- fromACN r1
+        (a3, r3) <- fromACN r2
+        (a4, r') <- fromACN r3
+        case r' of
+            AcnSeqEnd:l -> return ((a1,a2,a3,a4), l)
+            _           -> Left "expecting AcnSeqEnd"
+instance (ToACN a1, ToACN a2, ToACN a3, ToACN a4, ToACN a5) => ToACN (a1,a2,a3,a4,a5) where
+    toACN (a1,a2,a3,a4,a5) = [AcnSeqStart] ++ toACN a1 ++ toACN a2 ++ toACN a3 ++ toACN a4 ++ toACN a5 ++ [AcnSeqEnd]
+instance (FromACN a1, FromACN a2, FromACN a3, FromACN a4, FromACN a5) => FromACN (a1,a2,a3,a4,a5) where
+    fromACN (AcnSeqStart:r) = do
+        (a1, r1) <- fromACN r
+        (a2, r2) <- fromACN r1
+        (a3, r3) <- fromACN r2
+        (a4, r4) <- fromACN r3
+        (a5, r') <- fromACN r4
+        case r' of
+            AcnSeqEnd:l -> return ((a1,a2,a3,a4,a5), l)
+            _           -> Left "expecting AcnSeqEnd"
+instance (ToACN a1, ToACN a2, ToACN a3, ToACN a4, ToACN a5, ToACN a6) => ToACN (a1,a2,a3,a4,a5,a6) where
+    toACN (a1,a2,a3,a4,a5,a6) = [AcnSeqStart] ++ toACN a1 ++ toACN a2 ++ toACN a3 ++ toACN a4 ++ toACN a5 ++ toACN a6 ++ [AcnSeqEnd]
+instance (FromACN a1, FromACN a2, FromACN a3, FromACN a4, FromACN a5, FromACN a6) => FromACN (a1,a2,a3,a4,a5,a6) where
+    fromACN (AcnSeqStart:r) = do
+        (a1, r1) <- fromACN r
+        (a2, r2) <- fromACN r1
+        (a3, r3) <- fromACN r2
+        (a4, r4) <- fromACN r3
+        (a5, r5) <- fromACN r4
+        (a6, r') <- fromACN r5
+        case r' of
+            AcnSeqEnd:l -> return ((a1,a2,a3,a4,a5,a6), l)
+            _           -> Left "expecting AcnSeqEnd"
