0b76dfc5498af904b4d5b977b2128dca2213765a
Vincent Hanquez <vhanquez@alephcloud.com>
2014-09-11 22:46:10 +0100

    add some helpers to serialize more quickly between bytestring and high level ACN'ed object.

--- AlephCloud/ACN.hs
+++ AlephCloud/ACN.hs
@@ -2,8 +2,19 @@ module AlephCloud.ACN
     ( module AlephCloud.ACN.Types
     , module AlephCloud.ACN.Event
     , module AlephCloud.ACN.Marshall
+    , acnObjectFromBytes
+    , acnObjectToBytes
     ) where
 
 import AlephCloud.ACN.Types
 import AlephCloud.ACN.Event
 import AlephCloud.ACN.Marshall
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as L
+
+acnObjectFromBytes :: FromACN a => B.ByteString -> Either String (a, [Acn])
+acnObjectFromBytes =
+    either (Left . snd) (maybe (Left "Invalid tagged val") fromACN . sequence . map fromTaggedVal) . runParse . L.fromStrict
+
+acnObjectToBytes :: ToACN a => a -> B.ByteString
+acnObjectToBytes a = toByteString $ map toTaggedVal $ toACN a
--- AlephCloud/ACN/Event/Encode.hs
+++ AlephCloud/ACN/Event/Encode.hs
@@ -1,11 +1,32 @@
 module AlephCloud.ACN.Event.Encode
     ( toLazyByteString
+    , fromLazyByteString
+    , toByteString
+    , fromByteString
     ) where
 
+import Control.Applicative
 import AlephCloud.ACN.Event.Types
 import Data.Binary.Put (runPut)
-import Data.Binary (put)
+import Data.Binary.Get (runGetIncremental, Decoder(..), isEmpty, pushChunks)
+import Data.Binary (put, get)
 import qualified Data.ByteString.Lazy as L
+import qualified Data.ByteString as B
 
 toLazyByteString :: [TaggedVal] -> L.ByteString
 toLazyByteString vals = runPut (mapM_ put vals)
+
+toByteString :: [TaggedVal] -> B.ByteString
+toByteString = L.toStrict . toLazyByteString
+
+fromLazyByteString :: L.ByteString -> Either String [TaggedVal]
+fromLazyByteString l = toResult (runGetIncremental loop `pushChunks` l)
+  where loop = do
+            b <- isEmpty
+            if b then return [] else (:) <$> get <*> loop
+        toResult (Fail _ _ s) = Left s
+        toResult (Partial _)  = Left "partial"
+        toResult (Done _ _ a) = Right a
+
+fromByteString :: B.ByteString -> Either String [TaggedVal]
+fromByteString = fromLazyByteString . L.fromStrict
